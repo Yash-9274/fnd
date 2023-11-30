b8202f4a496110a3be86c8e0b8ab29302143cf2d
IAndy234 <78110729+IAndy234@users.noreply.github.com>
2023-12-01 02:01:08 +0530

    dock

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 import unittest
 from flask import Flask
-from unittest.mock import patch
 from app import app
 
 class FlaskAppTest(unittest.TestCase):
@@ -22,22 +21,6 @@ class FlaskAppTest(unittest.TestCase):
         response = self.app.post('/verify', data={'verification_text': verification_text})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Text Verification Result', response.data)
-
-    @patch('app.requests.get')  # Mocking the requests.get method
-    def test_api_calling(self, mock_get):
-        # Mock the response from the News API
-        mock_get.return_value.json.return_value = {
-            'articles': [
-                {'title': 'Fake News Article', 'description': 'This is a fake news article.'},
-                {'title': 'Real News Article', 'description': 'This is a real news article.'}
-            ]
-        }
-
-        # Call the API method
-        result = app.fetch_news_articles('example')
-
-        # Assert that the result is as expected
-        self.assertTrue(result)  # Assuming the result is True if any article contains the verification text
 
     def tearDown(self):
         pass
