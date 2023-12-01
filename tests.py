08be2e9420f8eb736d4c6ba0f3742df0177e3695
IAndy234 <78110729+IAndy234@users.noreply.github.com>
2023-12-01 16:51:45 +0530

    tests

--- tests.py
+++ tests.py
@@ -1,25 +1,29 @@
-User
-# tests.py
 import unittest
+from flask import Flask
 from app import app
 
-class FlaskAppTests(unittest.TestCase):
+class FlaskAppTest(unittest.TestCase):
+
     def setUp(self):
-        app.config['TESTING'] = True
+        # Create a test client
         self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_route(self):
+        # Test the home route
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Fake News Detection', response.data)
+
+    def test_verify_text_route(self):
+        # Test the verify text route with a sample verification text
+        verification_text = 'example'
+        response = self.app.post('/verify', data={'verification_text': verification_text})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Text Verification Result', response.data)
 
     def tearDown(self):
         pass
-
-    def test_home_route(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'News Articles', response.data)
-
-    def test_verify_text_route(self):
-        response = self.app.post('/verify', data={'verification_text': 'fake'})
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Text Verification Result', response.data)
 
 if __name__ == '__main__':
     unittest.main()
