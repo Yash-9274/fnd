23dc2a46ac0bbd1a988fe9a315c26787b1b56614
IAndy234 <78110729+IAndy234@users.noreply.github.com>
2023-12-01 16:40:42 +0530

    test

--- test.js
+++ test.js
@@ -1,26 +1,25 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
 async function runTest() {
   // Set up the WebDriver with Chrome
-  let driver = await new Builder()
-    .forBrowser('chrome')
-    .build();
+  let driver = await new Builder().forBrowser("chrome").build();
 
   try {
     // Navigate to your website
     await driver.get('http://127.0.0.1:8000/');
+    await driver.wait(until.elementLocated(By.name('q')), 10000); // Wait for up to 10 seconds
+
 
     // Wait for the search box to be present
-    let searchBox = await driver.wait(until.elementLocated(By.name('q')), 5000);
+    let searchBox = await driver.wait(until.elementLocated(By.name("q")), 5000);
 
     // Interact with the search box
-    await searchBox.sendKeys('Selenium', Key.RETURN);
+    await searchBox.sendKeys("Selenium", Key.RETURN);
 
     // Wait for a specific condition (e.g., title to be a certain value)
-    await driver.wait(until.titleIs('Selenium - Google Search'), 5000);
+    await driver.wait(until.titleIs("Selenium - Google Search"), 5000);
 
     // Additional actions can be added here
-
   } finally {
     // Optionally, you can close the browser here
     // await driver.quit();
